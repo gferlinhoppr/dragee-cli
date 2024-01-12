480a3196a028e9747b82d7cf92a2bd408acd8ccd
Guillaume Ferlin <gferlin@hoppr.tech>
2024-01-12 10:50:24 +0100

    Refactor test

--- test/dragee-cli.spec.ts
+++ test/dragee-cli.spec.ts
@@ -3,19 +3,19 @@ import {expect, test, describe} from "bun:test";
 const asserter = (dragees: Dragee[]) => {
     const aggregates = dragees.filter(dragee => dragee.kind_of === 'ddd/aggregate');
     const services = dragees.filter(dragee => dragee.kind_of === 'ddd/service');
+    const dependenciesOf = (dragee: Dragee) => {
+        const depends = Object.keys(dragee.depends_on);
+        return dragees.filter(dragee => depends.includes(dragee.name));
+    }
+
     describe('Namespace: DDD', () => {
         test.each(dragees)('Dragee %p', (dragee: Dragee) => {
             expect(dragee).not.toBeUndefined();
         });
 
         describe('Check aggregate rules', () => {
-            
-            test.each(aggregates)('Aggregate %p', aggregate => {
-                const depends = Object.keys(aggregate.depends_on);
-
-                const dependenciesDragees = dragees.filter(dragee => depends.includes(dragee.name));
-                
-                dependenciesDragees.forEach( (aggregateDependency: Dragee) => {
+            test.each(aggregates)('Aggregate %p', aggregate => {                
+                dependenciesOf(aggregate).forEach( (aggregateDependency: Dragee) => {
                     expect(['ddd/value_object', 'ddd/entity']).toContain(aggregateDependency.kind_of);
                 });
             });
@@ -23,11 +23,7 @@ const asserter = (dragees: Dragee[]) => {
         });
         describe('Check service rules', () => {
             test.each(services)('Service %p', service => {
-                const depends = Object.keys(service.depends_on);
-
-                const dependenciesDragees = dragees.filter(dragee => depends.includes(dragee.name));
-                
-                dependenciesDragees.forEach( (serviceDependency: Dragee) => {
+                dependenciesOf(service).forEach( (serviceDependency: Dragee) => {
                     expect(serviceDependency.kind_of).toBe('ddd/repository');
                 });
             });
