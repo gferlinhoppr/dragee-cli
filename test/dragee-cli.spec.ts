254d4aee980826f79228f833866c25edba689591
Guillaume Ferlin <gferlin@hoppr.tech>
2024-01-12 10:10:43 +0100

    first test aggregate

--- test/dragee-cli.spec.ts
+++ test/dragee-cli.spec.ts
@@ -1,25 +1,66 @@
 import {expect, test, describe} from "bun:test";
 
 const asserter = (dragees: Dragee[]) => {
+    const aggregates = dragees.filter(dragee => dragee.kind_of === 'ddd/aggregate');
     describe('Namespace: DDD', () => {
         test.each(dragees)('Dragee %p', (dragee: Dragee) => {
             expect(dragee).not.toBeUndefined();
         });
 
         describe('Check aggregate rules', () => {
-            test.each(dragees)('Dragee %p', (dragee: Dragee) => {
-                expect(dragee).not.toBeUndefined();
+            
+            test.each(aggregates)('Aggregate %p', aggregate => {
+                const depends = Object.keys(aggregate
+                    .depends_on);
+
+                const dependenciesDragees = dragees.filter(dragee => depends.includes(dragee.name));
+                
+                dependenciesDragees.forEach
+                ( (aggregateDependency: Dragee) => {
+                    expect(aggregateDependency.kind_of).toBe('ddd/value_object');
+                });
+
             });
+            
+            // const startTime = new Date();
+            // while (new Date().getTime() - startTime.getTime() < 10000) {}
+            // debugger;
+
         });
     })
 }
 
-asserter([{
-    name: 'toto',
-    kind_of: 'toto/titi',
-    depends_on: []
-}, {
-    name: 'tutu',
-    kind_of: 'toto/titi',
-    depends_on: []
-}]);
+asserter(
+    [
+        {
+            name: 'toto',
+            kind_of: 'ddd/aggregate',
+            depends_on:
+                {
+                    'tutu': [
+                        'field'
+                    ] ,
+                    'titi': [
+                        'field'
+                    ] ,
+                    'foo': [
+                        'field'
+                    ]
+                    
+                }
+        }, 
+        {
+            name: 'tutu',
+            kind_of: 'ddd/value_object',
+            depends_on: []
+        },
+        {
+            name:'titi',
+            kind_of: 'ddd/value_object'
+        },
+        {
+            name:'foo',
+            kind_of: 'ddd/entity'
+        }
+    ]
+);
