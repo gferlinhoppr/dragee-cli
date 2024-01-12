8ff09ede7a2340943b54cb1961e01e1e9e872008
Guillaume Ferlin <gferlin@hoppr.tech>
2024-01-12 16:38:48 +0100

    refactor

--- src/ddd-asserter.ts
+++ src/ddd-asserter.ts
@@ -1,5 +1,5 @@
 export const dddAsserter = ((dragees: Dragee[]) => {
-    const proc = Bun.spawn(["bun", "test", "../test/dragee-cli.spec.ts"], 
+    const proc = Bun.spawn(["bun", "test", "../test/ddd-asserter-rules.spec.ts"], 
         {
             env: {
                 dragees: JSON.stringify(dragees)
